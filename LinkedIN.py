2d9e460f68e676a7570bc52c1d2b9aeb03cf1f03
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-06-30 00:53:49 -0300

    Add files via upload

---
+++ LinkedIN.py
@@ -0,0 +1,12 @@
+def print_alpha_nums(abc_list, num_list):
+	for char in abc_list:
+		for num in num_list:
+			print(char, num)
+	return
+
+# ---
+print_alpha_nums(['a', 'b', 'c'], [1, 2, 3])
+
+
+What is the correct syntax for creating variable that is bound to a list?
+>>>> my_list = [2, 'apple', 3.5]
