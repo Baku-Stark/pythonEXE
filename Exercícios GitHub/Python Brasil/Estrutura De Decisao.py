60af571d6a426f3a42a4c61627826349dd3728c1
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-07 19:56:08 -0300

    Update Estrutura De Decisao.py

--- Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
+++ Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
@@ -1,1 +1,12 @@
 Faça um Programa que peça dois números e imprima o maior deles.
+
+num1 = float(input('Digite o 1° número: '))
+num2 = float(input('Digite o 2° número: '))
+print('')
+if num1 > num2:
+	print('{} é maior que {}'.format(num1, num2))
+else:
+	print('{} é maior que {}'.format(num2, num1))
+  
+_________________________________________________________________________
+Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo. 
