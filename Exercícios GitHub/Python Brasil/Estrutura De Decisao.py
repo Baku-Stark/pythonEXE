f6808812912bde5e32c186ca4c2c27641a4aed18
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-11 21:45:03 -0300

    Update Estrutura De Decisao.py

--- Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
+++ Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
@@ -96,3 +96,57 @@ elif num3 < num1 and num3 < num2:
 print('-=' *30)
 
 _________________________________________________________________________
+Faça um programa que pergunte o preço de três produtos e informe qual produto 
+você deve comprar, sabendo que a decisão é sempre pelo mais barato. 
+
+def preco_Produto(ptd, ptd2, ptd3):
+	print('')
+	if ptd < ptd2 and ptd < ptd3:
+		print('\a O produto de R${:.2f} é o mais barato.'.format(ptd))
+	elif ptd2 < ptd and ptd2 < ptd3:
+		print('\a O produto de R${:.2f} é o mais barato.'.format(ptd2))
+	elif ptd3 < ptd and ptd3 < ptd2:
+		print('\a O produto de R${:.2f} é o mais barato.'.format(ptd3))
+
+
+###
+ptd_1 = float(input('Preço Do 1° Produto \nr: '))
+ptd_2 = float(input('Preço Do 2° Produto \nr: '))
+ptd_3 = float(input('Preço Do 3° Produto \nr: '))
+preco_Produto(ptd_1, ptd_2, ptd_3)
+
+_________________________________________________________________________
+Faça um Programa que leia três números e mostre-os em ordem decrescente. 
+
+listanum = []
+for n in xrange(1, 4):
+	listanum.append(int(input('Digite o {}° número \nr: '.format(n))))
+	print('')
+listanum.sort(reverse=True)
+print('Os números na ordem descrescente: {}'.format(listanum))
+
+_________________________________________________________________________
+Faça um Programa que pergunte em que turno você estuda. Peça para digitar 
+'M-matutino' ou 'V-Vespertino' ou 'N-Noturno'. Imprima a mensagem 
+"Bom Dia!", "Boa Tarde!" ou "Boa Noite!" ou "Valor Inválido!", conforme o caso.
+
+def dia_Turno(turno):
+	print('')
+	if turno == 'MANHÃ':
+		print('\a Turno da Manhã: BOM DIA!')
+	elif turno == 'TARDE':
+		print('\a Turno da Tarde: BOA TARDE!')
+	elif turno == 'NOITE':
+		print('\a Turno da Noite: BOA NOITE!')
+	else:
+		print('[ERRO] Resposta Inválida!')
+
+
+###
+dia = str(input('''Turno De Estudo
+[MANHÃ]
+[TARDE]
+[NOITE]
+
+\nr: ''')).upper()
+dia_Turno(dia)
