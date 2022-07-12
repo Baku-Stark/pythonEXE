7b839cc15fa1ee33206b740f0052ce26f79692a7
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-12 20:02:17 -0300

    Update Estrutura De Decisao.py

--- Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
+++ Exercícios GitHub/Python Brasil/Estrutura De Decisao.py
@@ -212,63 +212,98 @@ print('    Novo Salário: R${:.2f}'.format(aumento))
 print('-=' *30)
 
 _________________________________________________________________________
-As Organizações Tabajara resolveram dar um aumento de salário aos seus 
-colaboradores e lhe contraram para desenvolver o programa que calculará os 
-reajustes.
+Faça um programa para o cálculo de uma folha de pagamento, sabendo que os 
+descontos são do Imposto de Renda, que depende do salário bruto 
+(conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde 
+a 11% do Salário Bruto, mas não é descontado (é a empresa que deposita). 
+O Salário Líquido corresponde ao Salário Bruto menos os descontos. 
+O programa deverá pedir ao usuário o valor da sua hora e a quantidade de 
+horas trabalhadas no mês. 
+	Desconto do IR: 
+	[] Salário Bruto até 900 (inclusive) - isento
 
-    Faça um programa que recebe o salário de um colaborador e o reajuste 
-    segundo o seguinte critério, baseado no salário atual:
-    []Salários até R$ 280,00 (incluindo) : aumento de 20%
+	[] Salário Bruto até 1500 (inclusive) - desconto de 5% 
 
-    []Salários entre R$ 280,00 e R$ 700,00 : aumento de 15%
-    
-    []Salários entre R$ 700,00 e R$ 1500,00 : aumento de 10%
-    
-    []Salários de R$ 1500,00 em diante : aumento de 5% 
-    
-    Após o aumento ser realizado, informe na tela:
-    []O salário antes do reajuste;
+	[] Salário Bruto até 2500 (inclusive) - desconto de 10% 
 
-    []O percentual de aumento aplicado;
-
-    []O valor do aumento;
-    
-    []O novo salário, após o aumento. 
+	[] Salário Bruto acima de 2500 - desconto de 20% 
+	Imprima na tela as informações, dispostas conforme o exemplo abaixo.
 
 print('-=' *30)
-print('   TABAJARA   ')
+print('   FOLHA DE PAGAMENTO   ')
 print('-=' *30)
 print('')
-salario = float(input('Salário Do Usuário R$'))
+dia = int(input('Dias Trabalhados \nr: '))
 print('')
-## <= 280 == 20%
-if salario <= 280:
-	percentual = 20
-	aumento = salario + (salario * 20/100)
-	valor_aumento = aumento - salario
-## > 280 <= 700 == 15%
-elif salario > 280 and salario <= 700:
-	percentual = 15
-	aumento = salario + (salario * 15/100)
-	valor_aumento = aumento - salario
-## > 700 <= 1500 == 10%
-elif salario > 700 and salario <= 1500:
-	percentual = 10
-	aumento = salario + (salario * 10/100)
-	valor_aumento = aumento - salario
-## > 1500 == 5%
-elif salario > 1500:
-	percentual = 5
-	aumento = salario + (salario * 5/100)
-	valor_aumento = aumento - salario
+sal_hora = float(input('Salário Por Hora \nR$'))
+salario = sal_hora * dia
+print('')
 print('-=' *30)
 print('TOTAL:')
 print('')
-print('--- Salário antes do reajuste: R${:.2f}'.format(salario))
-print('--- Percentual de aumento aplicado: {}%'.format(percentual))
-print('--- O valor do aumento: R${:.2f}'.format(valor_aumento))
+## INSENTO
+if salario <= 900:
+	print('Salário Insento!')
+	print('Salário Bruto:  R${:.2f}'.format(salario))
+	salario_liq = salario
+
+## IR (5%) + INSS (10%)
+elif salario > 900 and salario <= 1500:
+	print('Desconto De 15% (IR + INSS)')
+	print('Salário Bruto:  R${:.2f}'.format(salario))
+	salario_liq = salario - (salario * 15/100)
+	tot_desconto = salario - salario_liq
+
+## IR (10%) + INSS (10%)
+elif salario > 1500 and salario <= 2500:
+	print('Desconto de 20% (IR + INSS)')
+	print('Salário Bruto:  R${:.2f}'.format(salario))
+	salario_liq = salario - (salario * 20/100)
+	tot_desconto = salario - salario_liq
+
+## IR (20%) + INSS (10%)
+elif salario > 2500:
+	print('Desconto de 30% (IR + INSS)')
+	print('Salário Bruto:  R${:.2f}'.format(salario))
+	salario_liq = salario - (salario * 30/100)
+	tot_desconto = salario - salario_liq
+
+## FGTS (11%)
+desc = salario - (salario * 11/100)
+fgts = salario - desc
+print('--- FGTS (11%): R${:.2f}'.format(fgts))
+print('--- Total De Descontos: R${:.2f}'.format(tot_desconto))
 print('')
-print('    Novo Salário: R${:.2f}'.format(aumento))
+print('    Salário Liquido: R${:.2f}'.format(salario_liq))
 print('-=' *30)
 
 _________________________________________________________________________
+Faça um Programa que leia um número e exiba o dia correspondente da semana. 
+(1-Domingo, 2- Segunda, etc.), se digitar outro valor deve aparecer valor inválido.
+
+print('''
+[ 1 ] -- Domingo
+[ 2 ] -- Segunda
+[ 3 ] -- Terça
+[ 4 ] -- Quarta
+[ 5 ] -- Quinta
+[ 6 ] -- Sexta
+[ 7 ] -- Sábado''')
+print('')
+semana = int(input('Escolha o dia da semana: '))
+if semana == 1:
+	print('\a DOMINGO')
+elif semana == 2:
+	print('\a SEGUNDA-FEIRA!')
+elif semana == 3:
+	print('\a TERÇA-FEIRA!')
+elif semana == 4:
+	print('\a QUARTA-FEIRA!')
+elif semana == 5:
+	print('\a QUINTA-FEIRA!')
+elif semana == 6:
+	print('\a SEXTA-FEIRA!')
+elif semana == 7:
+	print('\a DOMINGO!')
+else:
+	print('\a [ERRO] Dia Da Semana Inválido!')
