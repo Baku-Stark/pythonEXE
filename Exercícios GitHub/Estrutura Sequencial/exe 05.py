a264c59605377eaaa561f7d43f2f7b54bc647f4d
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-14 17:23:43 -0300

    Metros para centímetros.

---
+++ Exercícios GitHub/Estrutura Sequencial/exe 05.py
@@ -0,0 +1,6 @@
+Faça um Programa que converta metros para centímetros.
+1m -> 100cm
+
+metros = float(input('Comprimento para ser convertido [m]: '))
+cm = metros * 100
+print('{:.1f} metros ---> {:.1f} centímetros'.format(metros, cm))
