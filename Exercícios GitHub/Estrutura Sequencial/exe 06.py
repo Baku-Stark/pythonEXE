24c1ec7e0e8922fc24fcadc4d69ee586a0eb438e
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-14 17:24:24 -0300

    Raio Do Círculo

---
+++ Exercícios GitHub/Estrutura Sequencial/exe 06.py
@@ -0,0 +1,7 @@
+Faça um Programa que peça o raio de um círculo, calcule e mostre sua área. 
+3.14 * r²
+
+raio = float(input('Registre o valor do raio: '))
+area = 3.14 * raio
+print('')
+print('>>> A área do círculo é de {:.1f}m²'.format(area))
