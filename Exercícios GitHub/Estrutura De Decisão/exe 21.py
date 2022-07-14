2fd752a9f7ac1c1b6292f31b00de34d54c3e177a
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-14 20:43:26 -0300

    Número Inteiro ou Decimal

---
+++ Exercícios GitHub/Estrutura De Decisão/exe 21.py
@@ -0,0 +1,11 @@
+'''
+Faça um Programa que peça um número e informe se o número é inteiro ou decimal. 
+Dica: utilize uma função de arredondamento. 
+'''
+
+num = float(input('Digite um número: '))
+print('')
+if num % 1 == 0:
+	print('\f - Número Inteiro')
+else:
+	print('\f - Número Decimal')
