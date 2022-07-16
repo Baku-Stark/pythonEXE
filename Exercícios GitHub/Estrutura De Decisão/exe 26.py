0dc80d7599710d75d0076f9a7e818ef9aeb13e5a
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-16 18:06:10 -0300

    Update exe 26.py

--- Exercícios GitHub/Estrutura De Decisão/exe 26.py
+++ Exercícios GitHub/Estrutura De Decisão/exe 26.py
@@ -1,5 +1,5 @@
 print('''
-           Até 5 Kg                Acima de 5 Kg
+                      Até 5 Kg                Acima de 5 Kg
 \a File Duplo      R$ 4,90 por Kg          R$ 5,80 por Kg --- [ 1 ]
 \a Alcatra         R$ 5,90 por Kg          R$ 6,80 por Kg --- [ 2 ]
 \a Picanha         R$ 6,90 por Kg          R$ 7,80 por Kg --- [ 3 ]
