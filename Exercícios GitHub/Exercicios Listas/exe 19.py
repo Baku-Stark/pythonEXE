6f746705bfbed8daf13c7ce53d0b0ca5cf2ece42
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-30 21:37:39 -0300

    Votação para  o melhor Sistema Op.

--- Exercícios GitHub/Exercicios Listas/exe 19.py
+++ Exercícios GitHub/Exercicios Listas/exe 19.py
@@ -13,13 +13,13 @@ As possíveis respostas são:
 5- Mac OS
 6- Outro
 
-Você foi contratado para desenvolver um programa que leia o resultado da enquete e 
-informe ao final o resultado da mesma. O programa deverá ler os valores até ser 
-informado o valor 0, que encerra a entrada dos dados. Não deverão ser aceitos valores 
-além dos válidos para o programa (0 a 5). Os valores referentes a cada uma das opções 
-devem ser armazenados num vetor. Após os dados terem sido completamente informados, 
-o programa deverá calcular a percentual de cada um dos concorrentes e informar o 
-vencedor da enquete. O formato da saída foi dado pela empresa, e é o seguinte:
+Você foi contratado para desenvolver um programa que leia o resultado da enquete e informe 
+ao final o resultado da mesma. O programa deverá ler os valores até ser informado o valor 0, 
+que encerra a entrada dos dados. Não deverão ser aceitos valores além dos válidos para o 
+programa (0 a 6). Os valores referentes a cada uma das opções devem ser armazenados num 
+vetor. Após os dados terem sido completamente informados, o programa deverá calcular a 
+percentual de cada um dos concorrentes e informar o vencedor da enquete. O formato da saída 
+foi dado pela empresa, e é o seguinte: 
 
 Sistema Operacional     Votos   %
 -------------------     -----   ---
@@ -31,51 +31,63 @@ Mac OS                    150    2%
 Outro                     150    2%
 -------------------     -----
 Total                    8800
+
+O Sistema Operacional mais votado foi o Unix, com 3500 votos, correspondendo a 40% dos votos.
 '''
+
 # === DADOS ===
-opcoes = ["Windows Server", "Unix", "Linux", "Netware" "Mac OS", "Outros"]
+opcoes = ['Windows Server', 'Unix', 'Linux', 'Netware', 'Mac OS', 'Outro']
 sistemas = [0] * 6
 
+# === LIMPAR TELA ===
+def clear():
+	print('\n' *10)
+
+
+# ---
+print('Qual o melhor Sistema Operacional para uso em servidores?')
 print('''
-1- Windows Server
-2- Unix
-3- Linux
-4- Netware
-5- Mac OS
-6- Outro
+[ 1 ] - Windows Server
+[ 2 ] - Unix
+[ 3 ] - Linux
+[ 4 ] - Netware
+[ 5 ] - Mac OS
+[ 6 ] - Outro
 ''')
 print('')
 while True:
-    while True:
-        opcao = int(input('Escolha uma opção de Sistema Operacional \nr: '))
-        print('')
-        if opcao > 6 or opcao < 0:
-            print('\033[32mOpção Inválida!\033[m')
-            print('')
-        
-        else:
-            break
-    
-    if opcao == 0:
-        break
-    sistemas[opcao - 1] = sistemas[opcao - 1] + 1
-print('Sistema Operacional     Votos     Porcentagem(%)')
-print('-' *60)
-count = 0
+	while True:
+		opcao = int(input('Escolha a sua opção \nr: '))
+		print('')
+
+		if opcao < 0 or opcao > 6:
+			print('\a Opção Inválida! Tente novamente')
+			print('-' *25)
+			print('')
+
+		else:
+			break
+
+	if opcao == 0:
+		break
+	sistemas[opcao - 1] = sistemas[opcao - 1] + 1
+
+print('Sistema Operacional   Votos   Porcentagem(%)')
+print('-' *45)
+cont = 0
 melhor = 0
-melhorSis = ''
 perc = 0
-
+melhorSis = ''
 for s in sistemas:
-    print(f'{opcoes[count]}     {s}     {(s * 100) / sum(sistemas):.2f} %')
-
-    if s > melhor:
-        melhor = s
-        melhorSis = opcoes[count]
-        perc = (s * 100) / sum(sistemas)
-    count += 1
-
-print('-' *60)
-print(f'Total: {sum(sistemas)}')
-print(f'''O Sistema Operacional mais votado foi o {melhorSis},
-com {melhor} votos, correspondendo a {perc:.2f} dos votos''')
+	print('{:>15}{:>12}   {:.2f}%'.ljust(40).format(opcoes[cont], s, (s * 100) / sum(sistemas)))
+	if s > melhor:
+		melhor = s
+		melhorSis = opcoes[cont]
+		perc = (s * 100) / sum(sistemas)
+	cont += 1
+print('-' *45)
+clear()
+print('\a Total: {}'.format(sum(sistemas)))
+print('\a O Sistema Operacional mais votador: {}'.format(melhorSis))
+print('\a Porcentagem de votos: {}'.format(perc))
+print('\a Votos: {}'.format(melhor))
