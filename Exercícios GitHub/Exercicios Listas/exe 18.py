79ea9ab13952d11a831dc5ec5e3ab21657938676
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-07-30 22:03:05 -0300

    Votação De Melhor Jogador

--- Exercícios GitHub/Exercicios Listas/exe 18.py
+++ Exercícios GitHub/Exercicios Listas/exe 18.py
@@ -1,22 +1,19 @@
 '''
-Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores 
-para saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o 
-desenvolvimento de um programa, que será utilizado pelas telefonistas, para a computação 
-dos votos. Sua equipe foi contratada para desenvolver este programa, utilizando a 
-linguagem de programação C++. Para computar cada voto, a telefonista digitará um número, 
-entre 1 e 23, correspondente ao número da camisa do jogador. Um número de jogador igual 
-zero, indica que a votação foi encerrada. Se um número inválido for digitado, o programa 
-deve ignorá-lo, mostrando uma breve mensagem de aviso, e voltando a pedir outro número. 
-Após o final da votação, o programa deverá exibir:
+Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores para 
+saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o desenvolvimento 
+de um programa, que será utilizado pelas telefonistas, para a computação dos votos. 
+Sua equipe foi contratada para desenvolver este programa, utilizando a linguagem de 
+programação C++. Para computar cada voto, a telefonista digitará um número, entre 1 e 23, 
+correspondente ao número da camisa do jogador. Um número de jogador igual zero, indica que 
+a votação foi encerrada. Se um número inválido for digitado, o programa deve ignorá-lo, 
+mostrando uma breve mensagem de aviso, e voltando a pedir outro número. Após o final da 
+votação, o programa deverá exibir: 
 
-    a) O total de votos computados;
-
-    b) Os númeos e respectivos votos de todos os jogadores que receberam votos;
-
-    c) O percentual de votos de cada um destes jogadores;
-
-    d) O número do jogador escolhido como o melhor jogador da partida, juntamente com o número 
-    de votos e o percentual de votos dados a ele
+	O total de votos computados;
+	Os númeos e respectivos votos de todos os jogadores que receberam votos;
+	O percentual de votos de cada um destes jogadores;
+	O número do jogador escolhido como o melhor jogador da partida, juntamente com o 
+	número de votos e o percentual de votos dados a ele.
 
 Observe que os votos inválidos e o zero final não devem ser computados como votos. 
 O resultado aparece ordenado pelo número do jogador. O programa deve fazer uso de arrays. 
@@ -24,9 +21,9 @@ O programa deverá executar o cálculo do percentual de cada jogador através de
 Esta função receberá dois parâmetros: o número de votos de um jogador e o total de votos. 
 A função calculará o percentual e retornará o valor calculado. Abaixo segue uma tela de 
 exemplo. O disposição das informações deve ser o mais próxima possível ao exemplo. 
-Os dados são fictícios e podem mudar a cada execução do programa. Ao final, o programa 
-deve ainda gravar os dados referentes ao resultado da votação em um arquivo texto no 
-disco, obedecendo a mesma disposição apresentada na tela.
+Os dados são fictícios e podem mudar a cada execução do programa. Ao final, o programa deve 
+ainda gravar os dados referentes ao resultado da votação em um arquivo texto no disco, 
+obedecendo a mesma disposição apresentada na tela. 
 
 Enquete: Quem foi o melhor jogador?
 
@@ -55,8 +52,6 @@ O melhor jogador foi o número 9, com 4 votos, correspondendo a 50% do total de 
 
 # === DADOS ===
 votos = [0]*23
-num = 1000
-tot_votos = 0
 
 # === LIMPAR TELA ===
 def clear():
@@ -67,41 +62,40 @@ def clear():
 print('\033[36mEscolha um número entre 1 e 23\033[m')
 print('\033[34mO número "0" encerra o programa\033[m')
 print('')
-while num != 0:
-    num = int(input('Escolha um jogador \nr: '))
-    print('')
-    if num > 23:
-        print('Número Inválido')
-        continue
-    
-    else:
-        votos[num-1] = votos[num-1] + 1
-        tot_votos += 1
+while True:
+	while True:
+		opcao = int(input('Escolha a sua opção de voto \nr: '))
+		print('')
+		if opcao < 0 or opcao > 23:
+			print('\a Opção Inválida! Tente novamente.')
+			print('-' *25)
+		else:
+			break
+	if opcao == 0:
+		break
+	votos[opcao - 1] = votos[opcao - 1] + 1
 
+print('-=' *30)
+cont = 0
 melhor = 0
+perc = 0
+melhorPly = ''
+
+print('Resultado Da Votação')
+print('')
+print('Jogador   Votos   Porcentagem(%)')
+print('-' *45)
+
+for s in votos:
+	print('{:>3}{:>12}   {:.2f}%'.ljust(35).format(votos[cont], s, (s * 100) / sum(votos)))
+	if s > melhor:
+		melhor = s
+		melhorPly = votos[cont]
+		perc = (s * 100) / sum(votos)
+	cont += 1
+print('-' *45)
 clear()
-print(f'Foram computados {tot_votos} votos!!')
-print('')
-print('Jogador  |   Votos   |    % de Votos   ')
-print('=' *40)
-for idx in range(len(votos)):
-    if votos[idx] > 0:
-        if idx >= 0:
-            if votos[idx] < 10:
-                print(f'  {idx+1}          {votos[idx]}          {(votos[idx]/tot_votos)*100:.2f}')
-            
-            else:
-                print(f'  {idx+1}          {votos[idx]}          {(votos[idx]/tot_votos)*100:.2f}')
-        
-        else:
-            if votos[idx] < 10:
-                print(f'  {idx+1}          {votos[idx]}          {(votos[idx]/tot_votos)*100:.2f}')
-            else:
-                print(f'  {idx+1}          {votos[idx]}          {(votos[idx]/tot_votos)*100:.2f}')
-    
-    if votos[idx] > votos[melhor]:
-        melhor = votos[melhor]
-        
-print('=' *40)
-print('')
-print(f'O melhor atleta foi o camisa {idx+1}, com {votos[idx]} votos, correspondendo a {(votos[idx]/tot_votos)*100:.2f} do total de votos.')
+print('Total: {}'.format(sum(votos)))
+print('\a O Sistema Operacional mais votador: {}'.format(melhorPly))
+print('\a Porcentagem de votos: {}'.format(perc))
+print('\a Votos: {}'.format(melhor))
