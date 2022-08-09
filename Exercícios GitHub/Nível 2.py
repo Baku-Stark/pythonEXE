9158a6fbce32bcef9e47f2396b352cea5cbac7be
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-08-09 01:27:28 -0300

    Update Nível 2.py

--- Exercícios GitHub/Nível 2.py
+++ Exercícios GitHub/Nível 2.py
@@ -569,3 +569,39 @@ else:
 print('')
 print('--- {} foi divisível {}x!'.format(num, tot))
 print('-=' *30)
+
+--------------------------------------------------------
+'''Exercício Python 53: Crie um programa que leia uma frase qualquer e diga se 
+ela é um palíndromo, desconsiderando os espaços. Exemplos de palíndromos:
+
+- APOS A SOPA, A SACADA DA CASA, A TORRE DA DERROTA, O LOBO AMA O BOLO, 
+ANOTARAM A DATA DA MARATONA.
+Exemplos de Palíndromos:
+	[] - Apos a sopa
+	[] - A sacada da casa
+	[] - A torre da derrota
+	[] - O lobo ama o bolo
+	[] - Anotaram a data da maratona
+'''
+
+frase = str(input('Digite uma frase: ')).upper().strip()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+print('')
+for letra in range(len(junto)-1, -1, -1):
+	inverso += junto[letra]
+print('')
+print('-=' *30)
+print('Resultado')
+print('')
+print('O inverso: \a {}'.format(inverso))
+print('A frase:   \a {}'.format(junto))
+print('')
+
+if inverso == junto:
+	print('A frase é um PALÍNDROMO!')
+
+else:
+	print('A frase não é um PALÍNDROMO!')
+print('-=' *30)
