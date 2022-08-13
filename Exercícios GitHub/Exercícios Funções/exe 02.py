5fe1155db65780f755d167d9c02dc0efdab33fa0
Baku Wall-e <103138773+Baku-Stark@users.noreply.github.com>
2022-08-13 20:19:07 -0300

    Imprimir números crescentes

--- Exercícios GitHub/Exercícios Funções/exe 02.py
+++ Exercícios GitHub/Exercícios Funções/exe 02.py
@@ -1,1 +1,27 @@
-...
+'''
+Faça um programa para imprimir:
+
+        1
+        1   2
+        1   2   3
+        .....
+        1   2   3   ...  n
+
+para um n informado pelo usuário. Use uma função que receba um valor n inteiro imprima 
+até a n-ésima linha. 
+'''
+
+def num_Crescente(n: int):
+	for linha in xrange(1, n + 1):
+		for coluna in xrange(1, linha +1):
+			print(coluna, end= '   ')
+		print('')
+
+
+# ---
+print('Triângulo com 1')
+num_Crescente(1)
+print('Triângulo com 2')
+num_Crescente(2)
+print('Triângulo com 3')
+num_Crescente(3)
