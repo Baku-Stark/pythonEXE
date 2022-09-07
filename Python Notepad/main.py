b376e73864a2dce2f08a2fb5aaea375cf8350e11
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-09-07 17:05:38 -0300

    Atualização

--- Python Notepad/main.py
+++ Python Notepad/main.py
@@ -2,54 +2,53 @@ from tkinter import *
 from tkinter import messagebox
 
 root = Tk()
-root.geometry("300x500")
+root.geometry("850x550")
 root.title("Notepad Python")
-root.configure(background='#000000')
-root.resizable(width=FALSE, height=FALSE)
+root.configure(background="#000000")
 
 # ----------------------------------------
 # cores
-bg_label = '#111111'
-btn = '#2C00A3'
-fg = '#f0f8ff'
+bg_color = '#111111'
+fg_color = '#f0f8ff'
 
 # ----------------------------------------
 # função
-def anotar():
-    note = str(note_bar.get())
+def salvarComo():
+    note = str(fileMenu['label'].get())
+
     if note == "":
-        messagebox.showwarning('AVISO!', 'VOCÊ PRECISA ESCREVER ALO PRIMEIRO.')
-    else:
-        with open("Notepad Python.txt", 'w') as notepad:
-            notepad.write(note)
-    for i in note:
-        label_note.insert(END, i)
+        messagebox.showwarning("AVISO!", "Você precisa digitar algo!")
+
 # ----------------------------------------
 # widget
-note_bar = Entry(
-    root, font=('Arial 15')
-)
+meuMenu = Menu(root)
 
-btn = Button(
-    root, text='Anotar!', command=anotar, font=('Arial 10 bold'), width=27,
-    bg=btn, relief=RAISED, overrelief=RIDGE
-)
+fileMenu = Menu(meuMenu, tearoff=0)
+fileMenu.add_command(label="Novo  - Ctrl + N")
+fileMenu.add_command(label="Abrir  - Ctrl + O")
+fileMenu.add_command(label="Salvar - Ctrl + S")
+fileMenu.add_command(label="Salvar como", command=salvarComo)
+fileMenu.add_separator()
+fileMenu.add_command(label="Sair")
+meuMenu.add_cascade(label="Arquivo", menu=fileMenu)
 
-label_note = Listbox(
-    root, bg=bg_label, font=('Arial 9'), fg=fg,
-    width=39, height=26
-)
+editMenu = Menu(meuMenu, tearoff=0)
+editMenu.add_command(label="Maiúsculo")
+editMenu.add_command(label="Minúsculo")
+meuMenu.add_cascade(label="Editar", menu=editMenu)
 
-scroll = Scrollbar(
-    root, 
+helpMenu = Menu(meuMenu, tearoff=0)
+helpMenu.add_command(label="Acessar código do programa")
+helpMenu.add_command(label="Autor")
+meuMenu.add_cascade(label="Ajuda", menu=helpMenu)
+# ----------------------
+boxNote = Text(
+    root, font=('Arial 10 bold'), bg=bg_color, fg=fg_color
 )
-scroll.config(command=label_note.yview)
 
 # ----------------------------------------
 # painel
-note_bar.grid(row=0, column=0)
-btn.grid(row=1, column=0)
-label_note.place(x=1, y=78)
-scroll.place(x=280, y=78)
+boxNote.pack()
 
+root.config(menu=meuMenu)
 root.mainloop()
