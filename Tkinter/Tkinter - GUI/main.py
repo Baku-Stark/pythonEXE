bc3b79cff1444f8515eb0a691f008e1d81838372
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-04 15:50:07 -0300

    update database

--- Tkinter/Tkinter - GUI/main.py
+++ Tkinter/Tkinter - GUI/main.py
@@ -1,4 +1,9 @@
 from tkinter import *
+from tkinter.ttk import Treeview
+
+# ----------------------------------------
+# IMPORTAÇÃO DA "view.py"
+from view import *
 
 # =============================================
 # CORES
@@ -25,8 +30,8 @@ class App(Frame):
         super().__init__()
         # =============================================
         # WIDGET
-        root.title("Python GUI")
-        root.geometry("800x500")
+        root.title("Python - Lista Telefônica")
+        root.geometry("570x500")
         root.iconbitmap("_img/pc.ico")
         root.resizable(width='false', height='false')
 
@@ -42,26 +47,61 @@ class App(Frame):
             command=self.DarkMode
         )
 
+        self.label_nome = Label(
+            root, text="Nome*", font=('Poppins 10 bold'),
+            bg=light_right, fg=light_fg)
+        self.insert_nome = Entry(root)
+
+        self.label_ddd = Label(
+            root, text="DDD*", font=('Poppins 10 bold'),
+            bg=light_right, fg=light_fg)
+        self.insert_ddd = Entry(root)
+
+        self.label_telefone = Label(
+            root, text="Telefone*", font=('Poppins 10 bold'),
+            bg=light_right, fg=light_fg)
+        self.insert_telefone = Entry(root)
+
         # =============================================
         # FRAME CENTRO
         self.frame_center = Frame(
             root, bg=light_bg, width=340, height=500
         )
 
-        # =============================================
-        # FRAME DIREITO
-        self.frame_right = Frame(
-            root, bg=light_right, width=230, height=500
+        self.btn_insert = Button(
+            root, text="Inserir", font=('Arial 15 bold'),
+            fg=light_fg, bg=light_left, relief='raised', overrelief='ridge'
+        )
+
+        self.btn_update = Button(
+            root, text="Atualizar", font=('Arial 15 bold'),
+            fg=light_fg, bg=light_left, relief='raised', overrelief='ridge'
+        )
+
+        self.btn_delete = Button(
+            root, text="Deletar", font=('Arial 15 bold'),
+            fg=light_fg, bg=light_left, relief='raised', overrelief='ridge'
         )
 
         # =============================================
         # PAINEL
         self.frame_left.grid(row=0, column=1)
         self.theme_btn.place(x=0, y=10)
+        self.label_nome.place(x=20, y=50)
+        self.insert_nome.place(x=20, y=70)
+        # -----------------
+        self.label_ddd.place(x=20, y=100)
+        self.insert_ddd.place(x=20, y=120)
+        # -----------------
+        self.label_telefone.place(x=20, y=150)
+        self.insert_telefone.place(x=20, y=170)
 
         self.frame_center.grid(row=0, column=2)
+        self.btn_insert.place(x=235, y=250)
+        self.btn_update.place(x=350, y=250)
+        self.btn_delete.place(x=480, y=250)
 
-        self.frame_right.grid(row=0, column=3)
+        self.show()
 
     # =============================================
     # FUNÇÕES
@@ -73,19 +113,70 @@ class App(Frame):
             self.theme_btn['fg'] = dark_fg
 
             self.frame_left['bg'] = dark_left
+            self.label_nome['bg'] = dark_left
+            self.label_nome['fg'] = dark_fg
+            self.label_ddd['bg'] = dark_left
+            self.label_ddd['fg'] = dark_fg
+            self.label_telefone['bg'] = dark_left
+            self.label_telefone['fg'] = dark_fg
+
             self.frame_center['bg'] = dark_bg
-            self.frame_right['bg'] = dark_right
+            self.btn_insert['bg'] = dark_left
+            self.btn_insert['fg'] = dark_fg
+            self.btn_update['bg'] = dark_left
+            self.btn_update['fg'] = dark_fg
+            self.btn_delete['bg'] = dark_left
+            self.btn_delete['fg'] = dark_fg
         
         else:
             self.theme_btn['bg'] = light_left
             self.theme_btn['fg'] = light_fg
 
             self.frame_left['bg'] = light_left
+            self.label_nome['bg'] = light_left
+            self.label_nome['fg'] = light_fg
+            self.label_ddd['bg'] = light_left
+            self.label_ddd['fg'] = light_fg
+            self.label_telefone['bg'] = light_left
+            self.label_telefone['fg'] = light_fg
+
             self.frame_center['bg'] = light_bg
-            self.frame_right['bg'] = light_right
+            self.btn_insert['bg'] = light_left
+            self.btn_insert['fg'] = light_fg
+            self.btn_update['bg'] = light_left
+            self.btn_update['fg'] = light_fg
+            self.btn_delete['bg'] = light_left
+            self.btn_delete['fg'] = light_fg
+
+    global list_tel
+
+    def show(self):
+        global list_tel
+        lista = acess()
+        
+        tabela_head = ['ID', 'Nome', 'DDD', 'Telefone']
+        
+        hd = ["nw", "nw", "nw", "nw"]
+        h = [30, 100, 40, 170]
+        n = 0
+
+        list_tel = Treeview(
+            root, selectmode="extended", columns=tabela_head, show="headings"
+        )
+
+        list_tel.place(x=230, y=0)
+
+        for col in tabela_head:
+            list_tel.heading(col, text=col.title(), anchor=CENTER)
+            list_tel.column(col, width=h[n], anchor=hd[n])
+
+            n += 1
+
+        for item in lista:
+            list_tel.insert('', 'end', values=item)
 
 # =============================================
 # ATIVAÇÃO DO ROOT
 if __name__ == '__main__':
     app = App(root)
-    app.mainloop()
+    app.master.mainloop()
