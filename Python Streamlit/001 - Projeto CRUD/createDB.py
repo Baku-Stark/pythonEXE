b159d0877cd3212cc27debbb6304f3e583c1667e
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-17 17:18:52 -0300

    update NEW CRUD
    🐍 | Projeto refeito

--- Python Streamlit/001 - Projeto CRUD/createDB.py
+++
@@ -1,7 +0,0 @@
-import sqlite3 as lite
-
-con = lite.connect('database/database.db')
-
-with con:
-    cur = con.cursor()
-    cur.execute("CREATE TABLE client_users(id INTEGER PRIMARY KEY AUTOINCREMENT, users_name TEXT, users_age TEXT, users_profession TEXT)")
