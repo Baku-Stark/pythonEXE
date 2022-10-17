e14b172834dac0eb76af4cefe77e790a95cf8f61
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-17 14:59:48 -0300

    update

--- Python Streamlit/001 - Projeto CRUD/main.py
+++ Python Streamlit/001 - Projeto CRUD/main.py
@@ -11,6 +11,22 @@ con = lite.connect('database/database.db')
 
 # =============================================
 # FUNÇÕES
+# ----- INSERÇÃO
+def inserirUser(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO client_users (users_name, users_age, users_profession) VALUES (?, ?, ?)"
+        cur.execute(query, i)
+
+def createUser(name, age, ocu):
+    nome = name
+    idade = age
+    profissao = ocu
+
+    lista = [nome, idade, profissao]
+
+    inserirUser(lista)
+    showTable()
 
 # ----- MOSTRAR USUÁRIOS
 def readTable():
@@ -28,30 +44,14 @@ def showTable():
     columnsTable = ['ID', 'Nome', 'Idade', 'Profissão']
     
     df = pd.DataFrame(data=readTable(), columns=columnsTable)
-    df
+    st.table(df)
 
-# ----- INSERÇÃO
-def inserirUser(i):
-    with con:
-        cur = con.cursor()
-        query = "INSERT INTO client_users (users_name, users_age, users_profession) VALUES  (?, ?, ?)"
-        cur.execute(query, i)
-
-def createUser(name, age, ocu):
-    nome = name
-    idade = age
-    profissao = ocu
-
-    lista = [nome, idade, profissao]
-
-    inserirUser(lista)
-    showTable()
+# ----- DELETAR
+def deleteUser(i):
+    pass
 
 # ----- ATUALIZAR
 def updateUser(i):
-    pass
-# ----- DELETAR
-def deleteUser(i):
     pass
 
 # =============================================
@@ -67,22 +67,23 @@ with st.form("FORM_USER"):
     input_ocuP = st.selectbox("Selecione a sua profissão", ops)
 
     input_btnS = st.form_submit_button("Enviar")
+    btn_spaceDelete_function = st.form_submit_button("Excluir")
+    btn_spaceUpdate_function = st.form_submit_button("Atualizar")
 st.write("_"*30)
 # =============================================
 # TABELA [mySQL -> Pandas]
 st.title("Cliente Cadastrados")
 showTable()
-with st.form("FUNCTION_USER"):
-    input_delete = st.form_submit_button("Deletar")
-    input_update = st.form_submit_button("Atualizar")
 
 # =============================================
 # CHAMAR FUNÇÃO [button=TRUE]
 if input_btnS:
     createUser(input_name, input_ageU, input_ocuP)
 
-if input_delete:
-    pass
+if btn_spaceDelete_function:
+    btn_spaceDelete_function = st.button("Excluído")
+    deleteUser(btn_spaceDelete_function)
 
-if input_update:
-    pass
+if btn_spaceUpdate_function:
+    btn_spaceUpdate_function = st.button("Atualizado")
+    updateUser(btn_spaceUpdate_function)
