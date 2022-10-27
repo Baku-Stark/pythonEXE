e9250454dfc82e9c4459089a28deaeb104415cea
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-27 14:14:20 -0300

    update widgets

--- Tkinter/CRUD 2/main.py
+++ Tkinter/CRUD 2/main.py
@@ -7,6 +7,9 @@ from tkinter import *
 bg_root = "#111111"
 fg_root = "#111111"
 
+bg_button = "#111111"
+fg_button = "#f0f8ff"
+
 frame_top_bottom = "#f0f8ff"
 hl_bg = "#9C9FA6"
 
@@ -26,10 +29,11 @@ class App():
     
     def tela(self):
         self.root.title("Python - Cadastro de Cliente")
+        self.root.iconbitmap("_img/natural.ico")
         self.root.geometry("700x500")
         self.root.configure(background=bg_root)
         self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=500, height=300)
+        self.root.minsize(width=500, height=400)
         self.root.resizable(width='true', height='true')
         
     # ===========================================
@@ -56,30 +60,66 @@ class App():
     def frameTop_Gadgets(self):
         # -------- BUTTONS --------
         self.st_Limpar = Button(
-            self.frameTop, text="Limpar",  font=('Arial 8 bold'), fg=fg_root, overrelief="ridge", relief='raised'
+            self.frameTop, text="Limpar",
+            font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
+            overrelief="ridge", relief='raised'
         )
 
         self.st_Buscar = Button(
-            self.frameTop, text="Buscar",  font=('Arial 8 bold'), fg=fg_root, overrelief="ridge", relief='raised'
+            self.frameTop, text="Buscar",
+            font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
+            overrelief="ridge", relief='raised'
         )
 
         self.st_Novo = Button(
-            self.frameTop, text="Novo",  font=('Arial 8 bold'), fg=fg_root, overrelief="ridge", relief='raised'
+            self.frameTop, text="Novo",
+            font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
+            overrelief="ridge", relief='raised'
         )
 
         self.st_Alterar = Button(
-            self.frameTop, text="Alterar",  font=('Arial 8 bold'), fg=fg_root, overrelief="ridge", relief='raised'
+            self.frameTop, text="Alterar",
+            font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
+            overrelief="ridge", relief='raised'
         )
 
         self.st_Apagar = Button(
-            self.frameTop, text="Apagar", font=('Arial 8 bold'), fg=fg_root, overrelief="ridge", relief='raised'
+            self.frameTop, text="Apagar",
+            font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
+            overrelief="ridge", relief='raised'
         )
         # -------- BUTTONS --------
 
         # -------- LABELS N' INPUTS --------
         self.codigo = Label(
-            self.frameTop, text="Código", font=('Arial 10 bold')
+            self.frameTop, text="Código",
+            font=('Arial 10 bold'), bg=frame_top_bottom
         )
+        self.codigo_entry = Entry(self.frameTop)
+
+        # ----- NOME
+        self.nome = Label(
+            self.frameTop, text="Nome",
+            font=('Arial 10 bold'), bg=frame_top_bottom
+        )
+        self.nome_entry = Entry(self.frameTop)
+        # ----- NOME
+
+        # ----- TELEFONE
+        self.telefone = Label(
+            self.frameTop, text="Telefone",
+            font=('Arial 10 bold'), bg=frame_top_bottom
+        )
+        self.telefone_entry = Entry(self.frameTop)
+        # ----- TELEFONE
+
+        # ----- CIDADE
+        self.cidade = Label(
+            self.frameTop, text="Cidade",
+            font=('Arial 10 bold'), bg=frame_top_bottom
+        )
+        self.cidade_entry = Entry(self.frameTop)
+        # ----- CIDADE
         # -------- LABELS N' INPUTS --------
 
         # =======================================
@@ -97,7 +137,17 @@ class App():
         # -------- BUTTONS --------
 
         # -------- LABELS N' INPUTS --------
-        self.codigo.place(relx=0.05, rely=0.05)
+        self.codigo.place(relx=0.05, rely=0.02)
+        self.codigo_entry.place(relx=0.05, rely=0.12, relwidth=0.1, relheight=0.13)
+
+        self.nome.place(relx=0.05, rely=0.40)
+        self.nome_entry.place(relx=0.05, rely=0.52, relwidth=0.80, relheight=0.13)
+
+        self.telefone.place(relx=0.05, rely=0.7)
+        self.telefone_entry.place(relx=0.05, rely=0.8, relwidth=0.30)
+
+        self.cidade.place(relx=0.45, rely=0.7)
+        self.cidade_entry.place(relx=0.45, rely=0.8, relwidth=0.30)
         # -------- LABELS N' INPUTS --------
 
 # ===============================================
---
+++ Tkinter/exe 36 option box.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+
+opts = OptionMenu(root, options)
+opts.pack()
+
+root.mainloop()
