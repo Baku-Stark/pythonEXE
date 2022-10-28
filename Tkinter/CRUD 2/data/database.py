352b59d35b04f614c564e9aeaf1f8be16e165f3f
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-10-28 14:16:33 -0300

    update (database Python)

---
+++ Tkinter/CRUD 2/data/database.py
@@ -0,0 +1,11 @@
+import sqlite3 as lite
+
+# ===============================================
+# CONEXÃO COM SQLITE3
+con = lite.connect("data/database.db")
+
+# ===============================================
+# CRIAÇÃO DA TABELA
+with con:
+    cur = con.cursor()
+    cur.execute("CREATE TABLE cadastro(id INTEGER PRIMARY KET AUTOINCREMENT, codigo TEXT, nome TEXT, telefone TEXT, cidade TEXT)")
--- Tkinter/CRUD 2/main.py
+++ Tkinter/CRUD 2/main.py
@@ -1,6 +1,7 @@
 # ===============================================
 # IMPORTAÇÕES
 from tkinter import *
+from tkinter.ttk import Treeview
 
 # ===============================================
 # CORES
@@ -14,15 +15,26 @@ frame_top_bottom = "#f0f8ff"
 hl_bg = "#9C9FA6"
 
 # ===============================================
+# FUNÇOES
+class Funcs():
+    # Limpar Conteúdo [st_Limpar]
+    def limpar_tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.telefone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+        
+# ===============================================
 # JANELA [config.]
 root = Tk()
 
-class App():
+class App(Funcs):
     def __init__(self):
         self.root = root
         self.tela()
         self.frame_tela()
         self.frameTop_Gadgets()
+        self.frameBottom_Gadgets()
         # =======================================
         # ROOT [mainloop]
         root.mainloop()
@@ -56,13 +68,13 @@ class App():
         self.frameBottom.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)
     
     # ===========================================
-    # BUTTONS
+    # Gadgets [Top_Frame]
     def frameTop_Gadgets(self):
         # -------- BUTTONS --------
         self.st_Limpar = Button(
             self.frameTop, text="Limpar",
             font=('Arial 8 bold'), bg=bg_button, fg=fg_button,
-            overrelief="ridge", relief='raised'
+            overrelief="ridge", relief='raised', command=self.limpar_tela
         )
 
         self.st_Buscar = Button(
@@ -149,6 +161,33 @@ class App():
         self.cidade.place(relx=0.45, rely=0.7)
         self.cidade_entry.place(relx=0.45, rely=0.8, relwidth=0.30)
         # -------- LABELS N' INPUTS --------
+    
+    # ===========================================
+    # Gadgets [Bottom_Frame]
+    def frameBottom_Gadgets(self):
+        colunas = ("ID", "Código", "Nome", "Telefone", "Cidade")
+        self.listaCli = Treeview(
+            self.frameBottom, height=3, columns=colunas, show='headings'
+        )
+
+        self.scrollY = Scrollbar(
+            self.frameBottom, orient='vertical'
+        )
+        self.scrollY['command'] = self.listaCli.yview
+        self.listaCli.configure(yscrollcommand=self.scrollY)
+
+        listaWidth = [1, 50, 200, 125, 125]
+        c = 0
+        
+        for col in colunas:
+            self.listaCli.heading(c, text=col)
+            self.listaCli.column(c, width=listaWidth[c])
+            c += 1
+    
+        # =======================================
+        # INSERÇÃO DA LISTA
+        self.listaCli.place(relx=0.01, rely=0.01, relwidth=0.96, relheight=0.98)
+        self.scrollY.place(relx=0.97, rely=0.01, relwidth=0.03, relheight=0.98)
 
 # ===============================================
 # EXECUÇÃO DA JANELA
