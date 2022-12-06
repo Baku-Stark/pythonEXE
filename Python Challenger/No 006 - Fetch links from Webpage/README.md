5379a1720855fa6fd8c22eac07ae94e341a12676
Baku Wall-e <wysthellyjessemata13@gmail.com>
2022-12-06 15:30:33 -0300

    🐍 Update: Mini-Projects

---
+++ Python Challenger/No 006 - Fetch links from Webpage/README.md
@@ -0,0 +1,7 @@
+# Fetch links from Webpage
+
+> **REQUISITOS**
+* Este script recupera todos os links de uma determinada página da Web e os salva como um arquivo txt.
+
+> **Bibliotecas utilizadas**
+* Nenhuma
---
+++ Python Challenger/No 006 - Fetch links from Webpage/main.py
@@ -0,0 +1,23 @@
+def saveLinks(site_name, link):
+    '''
+    'r'  -> Usado somente para ler algo;
+    'r+' -> Usado para LER e ESCREVER algo;
+    'w'  -> Usado somente para escrever algo;
+    'w+' -> escrita (o modo w+, assim como o w, também apaga o conteúdo anterior do arquivo);
+    'a'  -> Usado para acrescentar algo;
+    'a+' -> leitura e escrita (abre o arquivo para atualização)
+    '''
+
+    if ("https" or "https") in link:
+        print('Endereçamento de domínio correto. Adicionado ao arquivo \033[32m[SaveLinks.txt]\033[m')
+        link_set = f"[{site_name}]({link})"
+        with open('SaveLinks.txt', 'a+') as arq:
+            arq.write(f"{link_set}\n")
+    else:
+        print('Endereçamento de domínio incorreto')
+
+# ===
+site_name = str(input('Nome do site : '))
+link = str(input('URL do site : '))
+print('')
+saveLinks(site_name, link)
