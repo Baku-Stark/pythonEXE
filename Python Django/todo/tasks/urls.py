0516a2d88e23b6dff5982b4864e57f83812ecfe3
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-02-08 19:34:56 -0300

    🐍 Update: App w/ Django

--- Python Django/todo/tasks/urls.py
+++ Python Django/todo/tasks/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.homePage),
     path('task-list/', views.taskList, name="task-list"),
     path('task/<int:task_id>', views.taskView, name="task-view-list"),
+    path('task-list/new_task/', views.createTask, name="new-task"),
     path('name/<str:name>', views.yourName)
 ]
--- Python Django/todo/tasks/views.py
+++ Python Django/todo/tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 # [MODELS]
 from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 def homePage(request):
@@ -10,7 +11,7 @@ def homePage(request):
 # RENDERIZAR O PRIMEIRO ARQUIVO HTML
 # LISTA DE TAREFAS [arquivo principal]
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('-created_at')
     return render(request, 'tasks/list.html', {'tasks' : tasks})
 
 # RENDERIZAR UMA VARIÁVEL
@@ -21,3 +22,18 @@ def yourName(request, name):
 def taskView(request, task_id):
     task = get_object_or_404(Task, pk=task_id)
     return render(request, 'tasks/task.html', {'task' : task})
+
+# CRIAR NOVA TAREFA
+def createTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/task-list/')
+
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/create.html', {'form' : form})
