67309a747f70c7e5a9e0476b7f26f1af8ef88746
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-01 17:32:17 -0300

    🐍 Update: Socket

--- Socket/002 - Socket Básico TCP - IP cliente e servidor/cpy.py
+++ Socket/002 - Socket Básico TCP - IP cliente e servidor/cpy.py
@@ -1,8 +1,12 @@
+import os
 from socket import *
 
 serv = socket(AF_INET, SOCK_STREAM)
 serv.connect((gethostname(), 55551))
 
 while True:
+    user = os.getlogin()
     msg = input('Message: ')
+
+    serv.send(user.encode())
     serv.send(msg.encode())
--- Socket/002 - Socket Básico TCP - IP cliente e servidor/spy.py
+++ Socket/002 - Socket Básico TCP - IP cliente e servidor/spy.py
@@ -8,12 +8,17 @@ print('=== F.R.I.D.A.Y SYSTEM===')
 
 while True:
     con, adr = serv.accept()
-    print(f"Connection {adr} has been established!")
+    
+    print(f"Connection (IP Private: {adr[0]}) has been established!")
+
     con.send(bytes("Welcome to F.R.I.D.A.Y server!!!", "utf-8"))
     print('')
 
     while True:
+        user = con.recv(1024)
+        user = user.decode()
+
         msg = con.recv(1024)
         msg = msg.decode()
-    
-        print(f'Client Server*: {msg}')
+
+        print(f'{user}*: {msg}')
