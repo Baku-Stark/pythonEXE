65a0efb8ff4072ca9c6b73969b5c1ab667c4b7da
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-04 19:17:16 -0300

    🐍 Update: Kivy Course

---
+++ Python Kivy/Kivy Course/007-Text-Input.py
@@ -0,0 +1,19 @@
+import os
+
+# KIVY
+try:
+    from kivy.app import App
+    from kivy.uix.gridlayout import GridLayout
+
+except ModuleNotFoundError:
+    os.system('python -m pip install "kivy[base]" --pre --extra-index-url https://kivy.org/downloads/simple/')
+# ====================== END OF KIVY
+
+class WidgetsExample(GridLayout):
+    pass
+
+class TheLab(App):
+    pass
+
+if __name__ == '__main__':
+    TheLab().run()
