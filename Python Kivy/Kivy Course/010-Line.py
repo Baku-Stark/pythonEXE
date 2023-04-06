759622c513b9c8e89ecc6bec4ccf42683a0fdaf7
Baku Wall-e <wysthellyjessemata13@gmail.com>
2023-04-06 14:55:35 -0300

    🐍 Update: Kivy Course

---
+++ Python Kivy/Kivy Course/010-Line.py
@@ -0,0 +1,22 @@
+import os
+
+# KIVY
+try:
+    from kivy.app import App
+    from kivy.uix.widget import Widget
+
+except ModuleNotFoundError:
+    os.system('python -m pip install "kivy[base]" --pre --extra-index-url https://kivy.org/downloads/simple/')
+# ====================== END OF KIVY
+
+class CanvasExample1(Widget):
+    pass
+
+class CanvasExample2(Widget):
+    pass
+
+class TheLab(App):
+    pass
+
+if __name__ == '__main__':
+    TheLab().run()
