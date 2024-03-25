80edcd05c7bece1bed94fc71a0e47066d01888e4
Baku Wall-e <wysthellyjessemata13@gmail.com>
2024-03-25 15:13:12 -0300

    🐍 Update: `Hacking`

--- H4cking/Beginner/ZIP CRACKER/script.py
+++ H4cking/Beginner/ZIP CRACKER/script.py
@@ -1,41 +1,51 @@
+import os
+
 # import necessary modules
 from pathlib import Path
 from zipfile import ZipFile
 from tqdm import tqdm #pip install tqdm - https://tqdm.github.io/
 
+from services.__COLORS import Colors
+from services.__TempPrint import TempPrint
+
+os.system('cls')
 # import the word-list
-word_list = Path(str(input('[0] Word List Path: ')))
+passwords_file = Path(str(input('[0] Word List Path: ')))
 zip_file_path = Path(str(input('[1] Zip File Path: ')))
 
-
 def crack_password():
-    # initialize the zip-file object
     zip_file = ZipFile(zip_file_path)
+    
+    # count the number of word in the file (pass.txt)
+    num_pass = len(list(open(passwords_file, 'rb')))
+    # print(num_pass)
 
-    # count the number of words in the word-list
-    n_words = len(list(open(word_list, 'rb')))
+    # open file and loop (for)
+    pass_file = open(passwords_file, 'rb')
 
-    # check how many passwords there are to test
-    print('[2] Total passwords to test:', f'{n_words:,}')
+    for password in tqdm(pass_file, total=num_pass, unit='password'):
+        TempPrint(Colors.CYAN + f"\n[ - ] Current password: {password.decode().strip()}"  + Colors.END).TPrint()
 
-    with open(word_list, 'rb') as wordlist:
-        for word in tqdm(wordlist, total=n_words, unit='word'):
-            try:
-                zip_file.extractall(pwd=word.strip())
+        try:
+            zip_file.extractall(pwd=password.strip(), path='output')
 
-            except:
-                continue
+        except Exception as error:
+            continue
+        
+        else:
+            print(Colors.GREEN + f'[ - ] Password found! [{password.decode().strip()}]' + Colors.END)
+            exit(0)
 
-            else:
-                print('\n[+] Password found:', word.decode().strip())
-                exit(0)
-
-    print("\n[!] Password not found, try other wordlist.")
+    print(Colors.RED + '[ x ] Password not found, try other wordlist...' + Colors.END)
 
 
-if word_list.exists() and zip_file_path.exists():
-    crack_password()
+if __name__ == '__main__':
+    if passwords_file.exists() and zip_file_path.exists():
+        print(Colors.GREEN + '[ - ] Path found!' + Colors.END)
+        print('')
+        
+        crack_password()
 
-else:
-    print('[x] An incorrect or non-existent path was entered')
-    exit(1)
+    else:
+        print(Colors.RED + '[ x ] An incorrect or non-existent path was entered...' + Colors.END)
+        exit(1)
