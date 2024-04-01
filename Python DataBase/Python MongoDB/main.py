ff2390b9367365d813d2ad1df26cb8289c581ac2
Baku Wall-e <wysthellyjessemata13@gmail.com>
2024-04-01 18:21:06 -0300

    🐍 Update: `Database`

--- Python DataBase/Python MongoDB/main.py
+++ Python DataBase/Python MongoDB/main.py
@@ -5,125 +5,96 @@ from dotenv import load_dotenv # pip install python-dotenv
 load_dotenv()
 
 from pymongo.mongo_client import MongoClient # python -m pip install "pymongo[srv]"==3.11
-mongo_acc = {
-    'user': os.getenv("MONGODB_USER"),
-    'password': os.getenv("MONGODB_PASSWORD")
-}
 
-uri = f"mongodb+srv://{mongo_acc['user']}:{mongo_acc['password']}@nodeapi.61bjodc.mongodb.net/?retryWrites=true&w=majority&appName=NodeAPI"
+class MongoDB_APP():
+    mongo_acc = {
+        'user': os.getenv("MONGODB_USER"),
+        'password': os.getenv("MONGODB_PASSWORD")
+    }
 
-# Create a new client and connect to the server
-client = MongoClient(uri)
-dbname = client['myListUsers']
-collections = dbname['users']
+    uri = f"mongodb+srv://{mongo_acc['user']}:{mongo_acc['password']}@nodeapi.61bjodc.mongodb.net/?retryWrites=true&w=majority&appName=NodeAPI"
 
+    # Create a new client and connect to the server
+    client = MongoClient(uri)
+    dbname = client['myListUsers']
+    collections = dbname['users']
 
-def create_db():
-    try:
-        process = True
-
-        name = str(input("What's your name?\nr: "))
-        print('')
-        age = int(input("What's your age?\nr: "))
-        print('')
-        job = []
-
-        while process:
-            job.append(
-                str(input("Type your job\nr: "))
-            )
+    def __init__(self):
+        # Send a ping to confirm a successful connection
+        try:
+            self.client.admin.command('ping')
+            print("Pinged your deployment. You successfully connected to MongoDB!")
+            print('-=' * 35)
             print('')
+            self.read_db()
+            print('-' * 20)
 
-            res = input("Finish (y/n)? ").upper()
-            if res == "Y":
-                process = False
-            else:
-                continue
+            # self.create_db()
+            
+            # self.delete_db()
+            # self.delete_db_by_id()
 
-        req = {'name': name, 'age': age, 'job': job}
-        cursor = collections.insert_one(req)
-        print(f"User created: {cursor.inserted_id}")
-    
-    except Exception as e:
-        message = f"An Exception occured when inserting a document into `{collections}` Collection. "
-        print(message, e)
+            # self.update_db()
 
-def read_db():
-    for item in collections.find():
-        print(item)
+        except Exception as e:
+            print(e)
 
-def update_db():
-    try:
-        data_id = str(input("Set an Item ID\nr: "))
 
-        new_values = { 
-            "$set" : {
-                'name' : "Python Update",
-                'age' : 90,
-                'job' : [
-                    'Easy Language',
-                    'Very Cool',
-                    'Nice Program'
-                ]
-            }
-        }
-
-        query = { '_id' : ObjectId(data_id) }
-        cursor = collections.update_one(query, new_values)
-
-        print(f"User Updated: {cursor.matched_count} - {data_id}")
-        print('')
-        read_db()
-
-    except Exception as e:
-        message = f"An Exception occured when inserting a document into `{collections}` Collection. "
-        print(message, e)
-
-def delete_db():
-    try:
-        name = str(input("Set an User\nr: "))
-        query = { 'name' : name }
-        cursor = collections.delete_one(query)
-        print('')
+    def create_db(self, req: object):
+        """
+            Mongo DB create function
+        """
+        try:
+            cursor = self.collections.insert_one(req)
+            print(f"User created: {cursor.inserted_id}")
         
-        print(f"User deleted: {cursor.deleted_count}")
-        print('')
-        read_db()
+        except Exception as e:
+            message = f"An Exception occured when inserting a document into `{self.collections}` Collection. "
+            print(message, e)
 
-    except Exception as e:
-        message = f"An Exception occured when inserting a document into `{collections}` Collection. "
-        print(message, e)
+    def read_db(self):
+        for item in self.collections.find():
+            print(item)
 
-def delete_db_by_id():
-    try:
-        data_id = str(input("Set an Item ID\nr: "))
-        query = { '_id' : ObjectId(data_id) }
-        cursor = collections.delete_one(query)
-        print('')
-        
-        print(f"User deleted: {cursor.deleted_count} - {data_id}")
-        print('')
-        read_db()
+    def update_db(self, data_id: str, req: object):
+        try:
+            new_values = { "$set" : req }
 
-    except Exception as e:
-        message = f"An Exception occured when inserting a document into `{collections}` Collection. "
-        print(message, e)
+            query = { '_id' : ObjectId(data_id) }
+            cursor = self.collections.update_one(query, new_values)
 
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-    print('-=' * 35)
-    print('')
-    read_db()
-    print('-' * 20)
+            print(f"User Updated: {cursor.matched_count} - ID: {data_id}")
+            print('')
+            self.read_db()
 
-    # create_db()
-    
-    # delete_db()
-    # delete_db_by_id()
+        except Exception as e:
+            message = f"An Exception occured when inserting a document into `{self.collections}` Collection. "
+            print(message, e)
 
-    update_db()
+    def delete_db(self, name: str):
+        try:
+            query = { 'name' : name }
+            cursor = self.collections.delete_one(query)
+            print('')
+            
+            print(f"User deleted: {cursor.deleted_count}")
+            print('')
+            self.read_db()
 
-except Exception as e:
-    print(e)
+        except Exception as e:
+            message = f"An Exception occured when inserting a document into `{self.collections}` Collection. "
+            print(message, e)
+
+    def delete_db_by_id(self, data_id: str):
+        try:
+            query = { '_id' : ObjectId(data_id) }
+            cursor = self.collections.delete_one(query)
+            print('')
+            
+            print(f"User deleted: {cursor.deleted_count} - {data_id}")
+            print('')
+            self.read_db()
+
+        except Exception as e:
+            message = f"An Exception occured when inserting a document into `{self.collections}` Collection. "
+            print(message, e)
